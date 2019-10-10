4b5a0a7eaef0587c6f37cb1f3e213659652d960c
Andrey Sechin <sechinandreyv@gmail.com>
2019-10-10 08:52:11 +0300

    login with devise and doorkeaper

--- Gemfile
+++ Gemfile
@@ -39,3 +39,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# OAuth 2 provider for Rails/Grape app
+gem "doorkeeper", "~> 5.2"
+
+# authentication solution for Rails based on Warden
+gem 'devise'
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable,
+  # :recoverable, :registerable
+  devise :database_authenticatable, :validatable
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  class << self
+    def authenticate(email, password)
+      user = User.find_for_authentication(email: email)
+      user.try(:valid_password?, password) ? user : nil
+    end
+  end
+
+end
